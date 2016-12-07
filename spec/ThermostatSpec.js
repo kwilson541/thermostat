f0384f7a5703ffd72ddc714519955fea2ce87106
Kim Wilson <k.wilson541@gmail.com>
2016-12-07 10:54:08 +0000

    Temperature can be increased or decreased

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,4 +8,15 @@ describe("Thermostat",function() {
   it("should have a default temperature of 20 degrees", function() {
     expect(thermostat.temperature).toEqual(20);
   });
+
+  it("should be able to increase the temperature by 1 degree", function() {
+  	thermostat.increaseTemperature();
+  	expect(thermostat.temperature).toEqual(21);
+  });
+
+  it("should be able to decrease the temperature by 1 degree", function() {
+  	thermostat.decreaseTemperature();
+  	expect(thermostat.temperature).toEqual(19);
+  });
+
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,4 +2,12 @@ function Thermostat() {
   this.temperature = defaultTemperature;
 }
 
+Thermostat.prototype.increaseTemperature = function() {
+	this.temperature += 1
+}
+
+Thermostat.prototype.decreaseTemperature = function() {
+	this.temperature -= 1
+}
+
 const defaultTemperature = 20;
